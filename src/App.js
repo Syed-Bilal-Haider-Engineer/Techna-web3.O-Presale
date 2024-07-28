2562ac09406b99672dad90d26715e39adea705b2
Syed BILAL Haider <87702448+Syed-Bilal-Haider-Engineer@users.noreply.github.com>
2024-07-28 23:17:49 +0500

    Update App.js
    use fragment.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import "./App.css";
-import Footer from "./Components/Footer/Footer";
-import Header from "./Components/Headers/Header";
-import TechnaSwap from "./Components/Swap/TechnaSwap";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React from 'react';
+import './App.css'; // Importing global styles
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Headers/Header';
+import TechnaSwap from './Components/Swap/TechnaSwap';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; // Importing Toastify styles
+
 function App() {
   return (
     <>
+      {/* Toast Container for notifications */}
       <ToastContainer
         position="top-right"
         autoClose={5000}
@@ -20,11 +23,10 @@ function App() {
         theme="light"
       />
 
-      <div>
-        <Header />
-        <TechnaSwap />
-        <Footer />
-      </div>
+      {/* Main application layout */}
+      <Header />
+      <TechnaSwap />
+      <Footer />
     </>
   );
 }
